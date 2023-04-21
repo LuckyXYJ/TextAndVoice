f8745f73e976ce83ae5b953dc82a79876664104b
邢雅杰 <470446087@qq.com>
2023-04-21 23:45:01 +0800

    Initial Commit

---
+++ TextAndVoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextAndVoice
+//
+//  Created by LuckyXYJ on 2023/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
